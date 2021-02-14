a341da442fa1bc1244b62101a8c4252236686045
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2021-02-15 00:15:50 +0530

    Update Program.cs

--- FindMinOrMaxTerm/Program.cs
+++ FindMinOrMaxTerm/Program.cs
@@ -24,6 +24,72 @@ namespace FindMinOrMaxTerm
             else
                 return third_Number;
         }
+        //UC3: Find max string from below
+ public string GetMaximumString(string FirstString, string SecondString, string ThirdString)
+        {
+            if (FirstString.CompareTo(SecondString) > 0 && FirstString.CompareTo(ThirdString) > 0)
+                return FirstString;
+            else if (SecondString.CompareTo(ThirdString) > 0)
+                return SecondString;
+            else
+                return ThirdString;
+        }
+
+//UC4:Extend the program
+
+    public class findMaxNum
+    {
+         public class findMaxNum<Extra> where Extra : IComparable
+    {
+        Extra[] Inputlist;
+
+
+        public findMaxNum(Extra[] Inputlist)
+        {
+            this.Inputlist = Inputlist;
+        }
+
+        public Extra[] SortArray(Extra[] Inputlist)
+        {
+            Array.Sort(Inputlist);
+            return Inputlist;
+        }
+
+        public Extra GetMaximumValue()
+        {
+            Extra[] SortedInputArray = SortArray(Inputlist);
+            return SortedInputArray.Last();
+        }
+
+//UC5:Extend program to the max
+ public class findMaxNum<Extra> where Extra : IComparable
+    {
+        Extra[] Inputlist;
+
+
+        public findMaxNum(Extra[] Inputlist)
+        {
+            this.Inputlist = Inputlist;
+        }
+     
+
+        public Extra[] SortArray(Extra[] Inputlist)
+        {
+            Array.Sort(Inputlist);
+            return Inputlist;
+        }
+
+        public void PrintMax(Extra[] SortedInputArray)
+        {
+            Console.WriteLine("Max Value: " + SortedInputArray.Last());
+        }
+
+        public Extra GetMaximumValue()
+        {
+            Extra[] SortedInputArray = SortArray(Inputlist);
+            PrintMax(SortedInputArray);
+            return SortedInputArray.Last();
+        }
 
 
 
