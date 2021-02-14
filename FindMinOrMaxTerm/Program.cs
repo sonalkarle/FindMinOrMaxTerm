281adf6e47e46e9ad82d62072c8f0eae905c75e7
sonal <sonalkarle01@gmail.com>
2021-02-14 22:40:04 +0530

    Find the maximum number from given number'

--- FindMinOrMaxTerm/Program.cs
+++ FindMinOrMaxTerm/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿
+using System;
 
 namespace FindMinOrMaxTerm
 {
-    class Program
+    public class findMaxNum
     {
+        public int GetMaximumInteger(int first_Number, int second_Number, int third_Number)
+        {
+            if (first_Number > second_Number && first_Number > third_Number)
+                return first_Number;
+            else if (second_Number > third_Number)
+                return second_Number;
+            else
+                return third_Number;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welome to find max term");
+
         }
     }
 }
