479ba3b2f93c192a298a3876aca5a7eb92e197ca
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2021-02-14 22:44:44 +0530

    Update UnitTest1.cs

--- FindMinOrMaxTerm/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
+++ FindMinOrMaxTerm/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -1,18 +1,37 @@
 using NUnit.Framework;
+using FindMinOrMaxTerm;
 
-namespace NUnitTestProject1
+namespace NUnitTestProject
 {
     public class Tests
     {
+        findMaxNum testMaximum;
+
         [SetUp]
         public void Setup()
         {
+            testMaximum = new findMaxNum();
+        }
+        //FIND MAXIMUM INTEGER
+        [Test]
+        public void GivenMaxInteger_AtFirstPosition_ShouldReturnTheSameNumber()
+        {
+            int result = testMaximum.GetMaximumInteger(88, 60, 44);
+            Assert.AreEqual(76, result);
         }
 
         [Test]
-        public void Test1()
+        public void GivenMaxInteger_AtSecondPosition_ShouldReturnTheSameNumber()
         {
-            Assert.Pass();
+            int result = testMaximum.GetMaximumInteger(70, 88, 14);
+            Assert.AreEqual(76, result);
+        }
+
+        [Test]
+        public void GivenMaxInteger_AtThirdPosition_ShouldReturnTheSameNumber()
+        {
+            int result = testMaximum.GetMaximumInteger(70, 54, 88);
+            Assert.AreEqual(76, result);
         }
     }
 }
