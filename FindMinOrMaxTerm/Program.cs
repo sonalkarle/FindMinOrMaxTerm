f249ea4e94191ebb98ca1da8b1fe7ec6d8d73af7
sonal <sonalkarle01@gmail.com>
2021-02-14 22:35:40 +0530

    Welcome

---
+++ FindMinOrMaxTerm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMinOrMaxTerm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welome to find max term");
+        }
+    }
+}
