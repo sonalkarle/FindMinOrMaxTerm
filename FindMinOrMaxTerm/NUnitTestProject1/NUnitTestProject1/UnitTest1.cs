6d69ab4d62eb5dd5d7698312c4d6890c4668c775
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2021-02-14 23:29:20 +0530

    Update UnitTest1.cs

--- FindMinOrMaxTerm/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
+++ FindMinOrMaxTerm/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -3,35 +3,37 @@ using FindMinOrMaxTerm;
 
 namespace NUnitTestProject
 {
-    public class Tests
+    public class findMaxNum
     {
-        findMaxNum testMaximum;
+        findMaxNum testMaximumnum;
 
         [SetUp]
         public void Setup()
         {
-            testMaximum = new findMaxNum();
+            testMaximumnum = new findMaxNum();
         }
-        //FIND MAXIMUM INTEGER
-        [Test]
-        public void GivenMaxInteger_AtFirstPosition_ShouldReturnTheSameNumber()
-        {
-            int result = testMaximum.GetMaximumInteger(88, 60, 44);
-            Assert.AreEqual(76, result);
-        }
+    
+      
+            public void GivenMaxFlaot_AtFirstPosition_ShouldReturnTheSameNumber()
+            {
+                float result = testMaximumnum.GetMaximumFlaot(88.1f, 60.4f, 55.8f);
+                Assert.AreEqual(88.1f, result);
+            }
 
-        [Test]
-        public void GivenMaxInteger_AtSecondPosition_ShouldReturnTheSameNumber()
-        {
-            int result = testMaximum.GetMaximumInteger(70, 88, 14);
-            Assert.AreEqual(76, result);
-        }
+            [Test]
+            public void GivenMaxFlaot_AtSecondPosition_ShouldReturnTheSameNumber()
+            {
+                float result = testMaximumnum.GetMaximumFlaot(60.4f, 88.1f, 55.3f);
+                Assert.AreEqual(88.1f, result);
+            }
 
-        [Test]
-        public void GivenMaxInteger_AtThirdPosition_ShouldReturnTheSameNumber()
-        {
-            int result = testMaximum.GetMaximumInteger(70, 54, 88);
-            Assert.AreEqual(76, result);
-        }
+            [Test]
+            public void GivenMaxFlaot_AtThirdPosition_ShouldReturnTheSameNumber()
+            {
+                float result = testMaximumnum.GetMaximumFlaot(54.2f, 60.3f, 88.1f);
+                Assert.AreEqual(88.1f, result);
+            }
     }
+    
+
 }
