32d9fe8e6dbc4b0260e4ea4ab256b3848beca6f1
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2021-02-14 22:51:56 +0530

    Update Program.cs

--- FindMinOrMaxTerm/Program.cs
+++ FindMinOrMaxTerm/Program.cs
@@ -14,6 +14,17 @@ namespace FindMinOrMaxTerm
             else
                 return third_Number;
         }
+        ///Uc2 :To find the max number in float numbers
+          public int GetMaximumInteger(int first_Number, int second_Number, int third_Number)
+        {
+            if (first_Number > second_Number && first_Number > third_Number)
+                return first_Number;
+            else if (second_Number > third_Number)
+                return second_Number;
+            else
+                return third_Number;
+        }
+
 
 
         static void Main(string[] args)
